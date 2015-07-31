306320e866cc490b7dd55a7e0a368253c4dc97f4
Andrew Mao <mizzao@gmail.com>
2015-07-31 15:16:41 -0400

    bots can take actions now

--- cooperation-loadtest.js
+++ cooperation-loadtest.js
@@ -4,18 +4,24 @@ sleep = Meteor.wrapAsync(function(time, cb) {
   }), time);
 });
 
-const num = 100;
+const num = Meteor.settings.clients;
 const batchId = Meteor.settings.batchId;
+const url = Meteor.settings.url;
 
 Meteor.startup(function() {
   const clients = [];
 
   for( let i = 0; i < num; i++ ) {
-    const client = DDP.connect("http://localhost:" + Meteor.settings.port);
+    const client = DDP.connect(url);
 
     // Set up collections for this client
     client.users = new Mongo.Collection("users", { connection: client });
     client.LobbyStatus = new Mongo.Collection("ts.lobby", { connection: client });
+
+    // Partitioned game collections
+    client.Actions = new Mongo.Collection('actions', { connection: client });
+    client.Rounds = new Mongo.Collection('rounds', { connection: client });
+    client.Games = new Mongo.Collection('games', { connection: client });
 
     // Wait till we're logged in to start doing stuff
     const loginHandle = client.users.find({}).observeChanges({
@@ -59,12 +65,13 @@ function startActions(client) {
   // Set up subscriptions to game data
   client.users.find({
     _id: client.userId,
-    "turkserver.group": {$exists: true}
+    group: { $exists: true }
   }, {
-    fields: { "turkserver.group": 1 }
+    fields: { group: 1 }
   }).observeChanges({
     added: function(userId, fields) {
-      setupSubscriptions(client, fields.turkserver.group);
+      teardownSubscriptions(client);
+      setupSubscriptions(client, fields.group);
     },
     removed: function() {
       teardownSubscriptions(client);
@@ -73,12 +80,33 @@ function startActions(client) {
 }
 
 function setupSubscriptions(client, group) {
-  teardownSubscriptions(client);
-
+  // TODO subscribe to other things like ts.rounds for load purposes
   client.userSub = client.subscribe('users', group);
   client.roundsSub = client.subscribe('rounds', group);
   client.actionsSub = client.subscribe('actions', group);
   client.gameSub = client.subscribe('games', group);
+
+  console.log("Subscribed to group");
+
+  client.actionHandle = client.Rounds.find({}).observeChanges({
+    added: function(id, fields) {
+      const index = fields.index;
+
+      console.log(`taking action for round ${index}`);
+
+      try {
+        client.call("chooseAction", Math.round(Math.random() + 1), index);
+      } catch (e) {
+        console.log("Couldn't take action for ", fields);
+      }
+
+      if (index === 10) {
+        sleep(500);
+        client.call("goToLobby");
+      }
+
+    }
+  });
 }
 
 function teardownSubscriptions(client) {
@@ -86,4 +114,6 @@ function teardownSubscriptions(client) {
     client[x] && client[x].stop();
     delete client[x];
   }
+
+  client.actionHandle && client.actionHandle.stop();
 }
